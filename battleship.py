e2a9c4baecd8950b139a1773291f9bda42ad036c
errolfeekery <errolfeekery@gmail.com>
2022-11-16 13:50:46 +0000

    add random number

--- battleship.py
+++ battleship.py
@@ -4,6 +4,9 @@ MY_GUESS = [[''] * 8 for x in range(8)]
 convert = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
 
+from random import randint
+
+
 def print_game(game): 
     print('    A B C D E F G H')
     print('    ---------------')
