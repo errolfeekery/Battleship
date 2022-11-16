e2050f561d74f089b73c356ce93541af89a5b12f
errolfeekery <errolfeekery@gmail.com>
2022-11-16 11:31:23 +0000

    add functions

--- battleship.py
+++ battleship.py
@@ -1,3 +1,18 @@
 MY_SHIPS = [[''] * 8 for x in range(8)]
 MY_GUESS = [[''] * 8 for x in range(8)]
 
+convert = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
+
+def game():
+    pass
+
+def guess_ship():
+    pass
+
+def make_ships():
+    pass
+
+def ship_hits():
+    pass
+
+
