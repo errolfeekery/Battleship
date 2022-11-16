48e76cb264ab230bcff087a130839f6620051667
errolfeekery <errolfeekery@gmail.com>
2022-11-16 14:15:33 +0000

    added row input to locate ship plus response to invalid input

--- battleship.py
+++ battleship.py
@@ -1,10 +1,10 @@
+from random import randint
+
+
 MY_SHIPS = [[''] * 8 for x in range(8)]
 MY_GUESS = [[''] * 8 for x in range(8)]
 
-convert = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
-
-
-from random import randint
+convert = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
 
 
 def print_game(game): 
@@ -16,16 +16,19 @@ def print_game(game):
         row_count += 1
 
 
-def guess_ship():
-    pass
-
-
 def make_ships(game):
     for boat in range(5):
         boat_row, boat_column = randint(0, 7), randint(0, 7)
         while game[boat_row][boat_column] == 'X':
             boat_row, boat_column = randint(0, 7), randint(0, 7)
         game[boat_row][boat_column] = 'X'
+
+
+def guess_ship():
+    row = input('Place ship row 1-8').lower()
+    while row not in '12345678':
+        pint('Enter a valid row')
+        row = input('Place ship row 1-8').lower()
 
 
 def ship_hits(): 
