0728c5c6c30be3018a1b3edcb9fbd2a9a0ef6787
errolfeekery <errolfeekery@gmail.com>
2022-11-16 11:38:06 +0000

    add make ship function

--- battleship.py
+++ battleship.py
@@ -4,7 +4,8 @@ MY_GUESS = [[''] * 8 for x in range(8)]
 convert = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
 def game():
-    pass
+     print('    A B C D E F G H')
+     print('    ---------------')
 
 def guess_ship():
     pass
@@ -15,4 +16,6 @@ def make_ships():
 def ship_hits():
     pass
 
-
+make_ships()
+goes = 10
+while goes > 0:
