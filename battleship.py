d00b93398ae96fc2b242f519dd79d76bdbde28a9
errolfeekery <errolfeekery@gmail.com>
2022-11-16 12:01:37 +0000

    fixed indendation not a multiple of four

--- battleship.py
+++ battleship.py
@@ -5,12 +5,12 @@ convert = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
 
 def print_game(game): 
-  print('    A B C D E F G H')
-  print('    ---------------')
-  row_count = 1
-  for row in game:
-          print("%d|%s|" % (row_count, "|".join(row)))
-          row_count += 1
+    print('    A B C D E F G H')
+    print('    ---------------')
+    row_count = 1
+    for row in game:
+        print("%d|%s|" % (row_count, "|".join(row)))
+        row_count += 1
 
 
 def guess_ship():
