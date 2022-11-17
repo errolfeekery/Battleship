9bde60b6e9a5d730fd5c39b911d4d7b51f292053
errolfeekery <errolfeekery@gmail.com>
2022-11-17 02:31:17 +0000

    fixed non appearing ships

--- battleship.py
+++ battleship.py
@@ -1,70 +1,75 @@
 from random import randint
 
 
-MY_SHIPS = [[''] * 8 for x in range(8)]
-MY_GUESS = [[''] * 8 for x in range(8)]
+HIDDEN_BOARD = [[' '] * 8 for x in range(8)]
+GUESS_BOARD = [[' '] * 8 for x in range(8)]
 
-convert = {'a': 0, 'b': 1, 'c': 2, 'd': 3, 'e': 4, 'f': 5, 'g': 6, 'h': 7}
-
-
-def print_game(game):
-    print('    A B C D E F G H')
-    print('    ---------------')
-    row_count = 1
-    for row in game:
-        print("%d|%s|" % (row_count, "|".join(row)))
-        row_count += 1
+letters_to_numbers = {'A': 0, 'B': 1, 'C': 2, 'D': 3, 'E': 4, 'F': 5, 'G': 6, 'H': 7}
 
 
-def make_ships(game):
-    for boat in range(5):
-        boat_row, boat_column = randint(0, 7), randint(0, 7)
-        while game[boat_row][boat_column] == 'X':
-            boat_row, boat_column = randint(0, 7), randint(0, 7)
-        game[boat_row][boat_column] = 'X'
+def print_board(board):
+    print('  A B C D E F G H')
+    print('  ---------------')
+    row_number = 1
+    for row in board:
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
 
 
-def guess_ship():
-    row = input('Place ship row 1-8')
+def create_ships(board):
+    for ship in range(5):
+        ship_row, ship_column = randint(0, 7), randint(0, 7)
+        while board[ship_row][ship_column] == 'X':
+            ship_row, ship_column = randint(0, 7), randint(0, 7)
+        board[ship_row][ship_column] = 'X'
+
+
+def get_ship_location():
+    row = input('Place ship row 1-8 ')
     while row not in '12345678':
-        pint('Enter a valid row')
+        print('Enter a valid row')
         row = input('Place ship row 1-8')
-    column = input('Place ship in column A-H').lower()
+    column = input('Place ship in column A-H').upper()
     while column not in 'ABCDEFGH':
         print('Enter a valid column')
-        column = input('Place ship in column A-H').lower()
-        return int(row) - 1, convert[column]
+        column = input('Place ship in column A-H').upper()
+        return int(row) - 1, letters_to_numbers[column]
 
 
-def ship_hits(game):
-    counter = 0
-    for row in game:
+def count_hits_ships(board):
+    count = 0
+    for row in board:
         for column in row:
             if column == 'X':
-                counter += 1
-        return counter
+                count += 1
+        return count
 
 
-make_ships(MY_SHIPS)
-goes = 10
-while goes > 0:
+create_ships(HIDDEN_BOARD)
+turns = 10
+print_board(HIDDEN_BOARD)
+print_board(GUESS_BOARD)
+
+""" while goes > 0:
     print('This is Battleship')
-    print_game(MY_GUESS)
-    row, column = guess_ship()
-    if MY_GUESS[row][column] == '-':
+    print_game(GUESS_BOARD)
+    row, column = get_boat_location()
+    if GUESS_BOARD[row][column] == '-':
         print('Already selected')
-    elif MY_SHIPS[row][column] == 'X':
+    elif GUESS_BOARD[row][column] == 'X':
         print('Direct hit!')
-        MY_GUESS[row][column] = "X"
+        GUESS_BOARD[row][column] = "X"
         goes -= 1
     else:
         print("Miss!")
-        MY_GUESS[row][column] = '-'
+        GUESS_BOARD[row][column] = '-'
         goes -= 1
-    if ship_hits(MY_GUESS) == 5:
+    if boat_hits(GUESS_BOARD) == 5:
         print('You sunk my battleships!')
         break
     print('You have ' + str(goes) + ' goes left')
     if goes == 0:
         print('Game is over!')
     break
+
+"""
